9ef23fd2f221ff7011afdee5054b0ab041771b4f
shadabali604 <shadabali604@gmail.com>
2019-12-09 22:28:16 +0530

    Google Auth20 Install And Imported

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send("Shadab");
-});
-app.get('/', (req, res) => {
-    res.send('Hey! This Shadab')
-})
+
+passport.use(new GoogleStrategy());
+
+
+
  
-const PORT  = process.env.PORT || 3000
+const PORT  = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log('Connecting')
 })
